ba395750f29a2a706bb940b827373e597992b4fe
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-10-17 22:14:03 +0900

    update store

---
+++ src/store/auth/index.js
@@ -0,0 +1,14 @@
+import state from './auth.state'
+import actions from './auth.actions'
+import getters from './auth.getters'
+import mutations from './auth.mutation'
+
+const auth = {
+  namespace: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
+
+export default auth
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './auth'
+import data from './data'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    auth,
+    data,
+  },
 })
