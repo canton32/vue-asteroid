3920af51086455b9c96e65cf91c98f2b0c35ab46
Anton Verhoeven <antonverhoeven32@gmail.com>
2020-10-21 21:20:35 +0800

    add axios init

--- src/main.js
+++ src/main.js
@@ -7,13 +7,22 @@ import router from './router'
 // store setup
 import store from './store'
 
+// plugins
 import plugins from './plugins'
+
+import axios from 'axios'
+
 
 // firebase setup
 import './firebase/init'
 import './firebase/authentication'
 
 Vue.config.productionTip = false
+
+// axios setup
+axios.defaults.baseURL = process.env.VUE_APP_NEOWS_BASE_URL
+axios.defaults.params = {}
+axios.defaults.params['api_key'] = process.env.VUE_APP_NEOWS_API_KEY
 
 // plugin setup
 Vue.use(plugins);
